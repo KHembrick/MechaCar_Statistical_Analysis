5cf396df49e51944d684e296c3efc629dd5b0879
KHembrick <96032255+KHembrick@users.noreply.github.com>
2022-04-24 12:40:51 -0400

    Updated R script

---
+++ MechaCarChallenge.Rscript2.R
@@ -0,0 +1,14 @@
+#Deliverable 1
+library(dplyr)
+
+MechaCar <- read.csv(file='MechaCar_mpg.csv', check.names=F, stringsAsFactors = F)
+
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar))
+
+#Deliverable 2
+SusCoil <- read.csv(file = 'Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+
+total_summary <- SusCoil %>% summarize(mean(PSI),median(PSI),var(PSI), sd(PSI), .groups = 'keep')
+
+lot_summary <- SusCoil %>% group_by(Manufacturing_Lot) %>% summarize(mean(PSI),median(PSI),var(PSI), sd(PSI), .groups= 'keep')
