b5fafe1cdd212ecb9e53f356c1082cce9b2bf363
KHembrick <96032255+KHembrick@users.noreply.github.com>
2022-04-24 13:42:33 -0400

    Updated Challenge Code

--- MechaCarChallenge.Rscript2.R
+++ MechaCarChallenge.Rscript2.R
@@ -12,3 +12,13 @@ SusCoil <- read.csv(file = 'Suspension_Coil.csv', check.names = F, stringsAsFact
 total_summary <- SusCoil %>% summarize(mean(PSI),median(PSI),var(PSI), sd(PSI), .groups = 'keep')
 
 lot_summary <- SusCoil %>% group_by(Manufacturing_Lot) %>% summarize(mean(PSI),median(PSI),var(PSI), sd(PSI), .groups= 'keep')
+
+#Deliverable 3
+t.test(SusCoil$PSI,mu=1500)
+
+#lot1
+t.test(subset(SusCoil$PSI,SusCoil$Manufacturing_Lot=='Lot1'),mu=1500)
+#lot2
+t.test(subset(SusCoil$PSI,SusCoil$Manufacturing_Lot=='Lot2'),mu=1500)
+#lot3
+t.test(subset(SusCoil$PSI,SusCoil$Manufacturing_Lot=='Lot3'),mu=1500)
